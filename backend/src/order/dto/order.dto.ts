018b94bc2747ec926a4d4c3943dc5af9f3ba3447
Фёдор Лодиков <fedor_ramoz@mail.ru>
2025-06-22 20:11:06 +0300

    fix: docker-compose.yml & fix:DTO

--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,16 +1,40 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateOrderRequestDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TicketDetailsDto)
   tickets: TicketDetailsDto[];
 }
 
 export class TicketDetailsDto {
+  @IsString()
+  @IsNotEmpty()
   film: string;
+
+  @IsString()
+  @IsNotEmpty()
   session: string;
-  daytime: string;
+
+  @IsNotEmpty()
   row: number;
+
+  @IsNotEmpty()
   seat: number;
-  price: number;
 }
 
 export class OrderConfirmationResponseDto {
